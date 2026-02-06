41b7ae42397d5fc11b1c286d9d75b6e839ca5fcf
Symfony Developer <dev@symfony.local>
2026-02-06 02:29:07 +0100

    crud + metires simple

--- update_existing_responses.sql
+++
@@ -1,4 +0,0 @@
--- Mise à jour des réponses existantes pour définir auteur_type = 'admin'
--- Toutes les réponses créées avant la migration n'ont pas de auteur_type
-
-UPDATE reponse SET auteur_type = 'admin' WHERE auteur_type IS NULL OR auteur_type = '';
